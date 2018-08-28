6cef66189a6b5c8d1e4b80304012321071f5775e
Navya567 <aretinavya@gmail.com>
2018-08-28 12:37:06 +0530

    Update CrudStudent.java

--- src/test/java/framework/CrudStudent.java
+++ src/test/java/framework/CrudStudent.java
@@ -15,7 +15,7 @@ import org.testng.annotations.Test;
 
 
 public class CrudStudent extends DriverFactory {
-	System.setProperty("webdriver.chrome.driver","<YourPath>chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver","C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
 	WebDriver driver = new ChromeDriver();
 	
 	
